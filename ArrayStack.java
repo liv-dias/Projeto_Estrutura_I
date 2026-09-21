6510638b2045f4fe95c6ed4ed6dc5e95736d7b57
liv-dias <10737709@mackenzista.com.br>
2026-09-21 17:53:21 -0300

    Add comments to ArrayStack
    Added comments in Portuguese to clarify the purpose of variables and methods.

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,9 +1,9 @@
 public class ArrayStack<E> implements MyStack<E>{
-    private E[] elements;
-    private int topoIndex;
+    private E[] elements;          // Vetor que armazena os elementos da pilha
+    private int topoIndex;         // Índice do elemento no topo (-1 quando vazia)
     private static final int DEFAULT_CAPACITY = 10;
 
-    
+    // Cria a pilha com capacidade fixa e inicia como vazia
     @SuppressWarnings("unchecked")
     public ArrayStack() {
         elements = (E[]) new Object[DEFAULT_CAPACITY];
@@ -13,10 +13,10 @@ public class ArrayStack<E> implements MyStack<E>{
     @Override 
     public void push(E element){
         if(topoIndex == elements.length - 1){
-            throw new IllegalStateException("Pilha cheia!");
+            throw new IllegalStateException("Pilha cheia!"); // Limite do vetor atingido
         }
         topoIndex++;
-        elements[topoIndex] = element;
+        elements[topoIndex] = element; // Insere no topo (LIFO)
     }
 
     @Override 
@@ -25,9 +25,9 @@ public class ArrayStack<E> implements MyStack<E>{
             throw new IllegalStateException("Pilha vazia!");
         }
         E element = elements[topoIndex];
-        elements[topoIndex] = null;
+        elements[topoIndex] = null;    // Evita referência presa (memory leak)
         topoIndex--;
-        return element;
+        return element;                // Remove e retorna o elemento do topo
     }
 
     @Override
@@ -35,7 +35,7 @@ public class ArrayStack<E> implements MyStack<E>{
         if(isEmpty()){
             throw new IllegalStateException("Pilha vazia!");
         }
-        return elements[topoIndex];
+        return elements[topoIndex];    // Consulta o topo sem remover
     }
 
     @Override 
@@ -55,6 +55,7 @@ public class ArrayStack<E> implements MyStack<E>{
             return;
         }
         System.out.println("Histórico de operações (MAIS ANTIGO AO MAIS RECENTE): ");
+        // Percorre do início do vetor até o topo, ou seja, do mais antigo ao mais recente
         for(int i = 0; i <= topoIndex; i++){
             System.out.println(elements[i]);
         }
